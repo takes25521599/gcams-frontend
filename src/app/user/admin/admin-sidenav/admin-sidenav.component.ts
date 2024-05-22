dddbdec1966cecd6ea26cd08eec34836e53ac7ad
takes <takeshi25521599@gmail.com>
2024-05-22 18:42:30 +0800

    Create admin components

---
+++ src/app/user/admin/admin-sidenav/admin-sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-sidenav',
+  templateUrl: './admin-sidenav.component.html',
+  styleUrl: './admin-sidenav.component.css'
+})
+export class AdminSidenavComponent {
+
+}
