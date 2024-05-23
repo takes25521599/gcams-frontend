eb68aa18d282113271ec4bae18f4ba1bf7fe64df
takes <takeshi25521599@gmail.com>
2024-05-24 03:12:50 +0800

    Update frontend

---
+++ src/app/user/admin/admin-write/admin-write.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-write',
+  templateUrl: './admin-write.component.html',
+  styleUrl: './admin-write.component.css'
+})
+export class AdminWriteComponent {
+}
